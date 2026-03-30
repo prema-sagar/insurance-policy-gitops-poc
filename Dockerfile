3cf24aa446a478ec7a3336c8fcb1f259956bbf5a
sagar <premasagar98@gmail.com>
2026-03-30 21:32:51 +0530

    Initial commit: working EAR + Docker + CI

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM open-liberty:full-java11-openj9
 
-COPY app/build/libs/*.ear /config/apps/
+COPY app/build/libs/insurance-policy-app.ear /config/apps/
 COPY app/src/main/liberty/config/server.xml /config/
 
 EXPOSE 9080 9443
--- app/src/main/java/com/company/insurance/soap/PolicyService.java
+++ app/src/main/java/com/company/insurance/soap/PolicyService.java
@@ -1,13 +1,18 @@
 package com.company.insurance.soap;
 
-import javax.jws.WebMethod;
 import javax.jws.WebService;
+import javax.jws.WebMethod;
 
 @WebService
 public class PolicyService {
 
     @WebMethod
-    public String getPolicyDetails(String policyNumber) {
-        return "Policy Details for: " + policyNumber;
+    public String getPolicy(String policyId) {
+        return "Policy details for " + policyId;
+    }
+
+    @WebMethod
+    public String healthCheck() {
+        return "OK";
     }
 }
