9260e9bb843d607c8bdd761ea473cc1f22970f64
prema-sagar <167697316+prema-sagar@users.noreply.github.com>
2026-04-07 21:41:00 +0530

    Create PolicyDetailsResponse.java

---
+++ src/main/java/com/example/insurance/soap/PolicyDetailsResponse.java
@@ -0,0 +1,60 @@
+package com.example.insurance.soap;
+
+import java.io.Serializable;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "PolicyDetailsResponse")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class PolicyDetailsResponse implements Serializable {
+
+    private String policyNumber;
+    private String insuredName;
+    private String policyType;
+    private String policyStatus;
+    private double premiumAmount;
+
+    public PolicyDetailsResponse() {
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public String getInsuredName() {
+        return insuredName;
+    }
+
+    public void setInsuredName(String insuredName) {
+        this.insuredName = insuredName;
+    }
+
+    public String getPolicyType() {
+        return policyType;
+    }
+
+    public void setPolicyType(String policyType) {
+        this.policyType = policyType;
+    }
+
+    public String getPolicyStatus() {
+        return policyStatus;
+    }
+
+    public void setPolicyStatus(String policyStatus) {
+        this.policyStatus = policyStatus;
+    }
+
+    public double getPremiumAmount() {
+        return premiumAmount;
+    }
+
+    public void setPremiumAmount(double premiumAmount) {
+        this.premiumAmount = premiumAmount;
+    }
+}
